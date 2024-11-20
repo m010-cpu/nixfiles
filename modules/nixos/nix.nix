0e8e77dc34b30c07c987c37e716e0c543045fd9b
mateo.ferreira <mateo.ferreira@epita.fr>
2024-11-20 22:18:31 +0100

    update nh

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -16,8 +16,8 @@
 
   programs.nh = {
     enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    # clean.enable = true;
+    # clean.extraArgs = "--keep-since 4d --keep 3";
     flake = "/home/mo/nixfiles";
   };
 }
