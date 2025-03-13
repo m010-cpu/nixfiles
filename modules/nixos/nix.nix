78d105aae0e8f8833e33c5ceabe53398edc9f51b
m010-cpu <81363508+m010-cpu@users.noreply.github.com>
2025-03-13 12:24:36 +0100

    add cachix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,8 +1,17 @@
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
 
   nixpkgs.config = {
     allowUnfree = true;
