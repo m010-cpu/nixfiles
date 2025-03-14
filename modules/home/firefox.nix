10266fbf7951e18cd48b650be47c44656f18b2e7
m010-cpu <81363508+m010-cpu@users.noreply.github.com>
2025-03-14 10:54:08 +0100

    update firefox

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -26,9 +26,9 @@
         x86_64-linux.tridactyl
       ];
 
-      # extraConfig = ''
-      #   ${builtins.readFile ../../arkenfox/user.js}
-      # '';
+      extraConfig = ''
+        ${builtins.readFile ../../arkenfox/user.js}
+      '';
 
       # arkenfox = {
       #   enable = true;
