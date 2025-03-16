28ef521b763fc6c9d131b52e8838cc058326f74c
m010-cpu <81363508+m010-cpu@users.noreply.github.com>
2025-03-16 11:10:33 +0100

    add kvmintel

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -23,7 +23,7 @@
 
   # boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_hardened;
   boot.kernel.sysctl."net.ipv4.icmp_echo_ignore_broadcasts" = lib.mkDefault true;
-  boot.kernelModules = ["uinput"];
+  boot.kernelModules = ["kvm_intel" "uinput"];
   boot.kernelParams = [
     "slab_nomerge"
     "page_poison=1"
