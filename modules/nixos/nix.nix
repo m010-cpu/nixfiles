b11efde81992618ba9fd440930b6207e1e2d75f9
m010-cpu <81363508+m010-cpu@users.noreply.github.com>
2025-04-07 18:19:38 +0200

    add stylix cachix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -10,12 +10,14 @@
       "https://hyprland.cachix.org"
       "https://firefox.cachix.org"
       "https://wezterm.cachix.org"
+      "https://stylix.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
       "firefox.cachix.org-1:KUx0AH19j+CuV2s5npUQjLlsVbyO6120hZWuDvZaEcs="
+      "stylix.cachix.org-1:iTycMb+viP8aTqhRDvV5qjs1jtNJKH9Jjvqyg4DYxhw="
     ];
   };
 
