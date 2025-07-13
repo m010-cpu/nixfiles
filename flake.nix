27d3f3f6f567e0107758615f1fa6c857fe600524
m010-cpu <81363508+m010-cpu@users.noreply.github.com>
2025-07-13 23:44:52 +0200

    update to stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/stable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     #nixos-hardware.url = "github:nixos/nixos-hardware/master";
     stylix.url = "github:danth/stylix";
 
