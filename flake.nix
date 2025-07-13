b08d8f682734452521a5f5062f8fe5046c461635
m010-cpu <81363508+m010-cpu@users.noreply.github.com>
2025-07-14 00:38:57 +0200

    add nixos-25.05 explicit

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/stable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+
     #nixos-hardware.url = "github:nixos/nixos-hardware/master";
     stylix.url = "github:danth/stylix";
 
