ec0171228fb09c3d8b3a629f1fd5abb46e04fe03
m010-cpu <81363508+m010-cpu@users.noreply.github.com>
2025-07-29 16:47:40 +0200

    fix vimtex with zathura

--- nvim/lua/m010-cpu/plugins/dev/vimtex.lua
+++ nvim/lua/m010-cpu/plugins/dev/vimtex.lua
@@ -1,14 +1,12 @@
 return {
-    "lervag/vimtex",
-    ft = "tex",
+  "lervag/vimtex",
+  ft = "tex",
 
-    config = function()
-        vim.g.vimtex_view_method = "skim"
-        vim.g.vimtex_view_skim_sync = 1
-        vim.g.vimtex_view_skim_activate = 1
+  config = function()
+    vim.g.vimtex_view_method = "zathura"
 
-        vim.g.vimtex_complete_close_braces = 1
-        vim.g.vimtex_quickfix_open_on_warning = 0
-        vim.g.vimtex_syntax_enabled = 0
-    end,
+    vim.g.vimtex_complete_close_braces = 1
+    vim.g.vimtex_quickfix_open_on_warning = 0
+    vim.g.vimtex_syntax_enabled = 0
+  end,
 }
